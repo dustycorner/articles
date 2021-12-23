d423b20c9f31df2ad771e56075408f95ea54c56a
Spencer Collyer <spencer@spencercollyer.plus.com>
2021-12-23 09:16:08 +0000

    Moved article from submitted to released repository

---
+++ c++20-text-formatting-introduction/build-all-std.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+if [ ! -d prgs ]
+then
+    mkdir prgs
+fi
+
+function buildit
+{
+    NAME=$1
+    cppfile=stdcode/${NAME}.cpp
+    prgfile=stdprgs/${NAME}
+    outfile=stdoutput/${NAME}.out
+    if [ ! -f $prgfile ]
+    then
+        prgtime="0"
+    else
+        prgtime=$(stat -c "%Y" $prgfile)
+    fi
+    cpptime=$(stat -c "%Y" $cppfile)
+    if [ $cpptime -gt $prgtime ]
+    then
+        echo "Building $NAME"
+        clang++ --std=c++20 $cppfile -o $prgfile
+        prgtime=$(stat -c "%Y" $prgfile)
+    else
+        echo "$NAME up-to-date"
+    fi
+    if [ ! -f $outfile ]
+    then
+        outtime="0"
+    else
+        outtime=$(stat -c "%Y" $outfile)
+    fi
+    if [ $prgtime -gt $outtime ]
+    then
+        echo "Creating $outfile"
+        $prgfile >$outfile
+    fi
+}
+
+buildit align-fill
+buildit bad-format
+buildit basic-formatting
+buildit bool-format
+buildit char-format
+buildit float-format
+buildit float-inf-nan
+buildit formatted_size
+buildit format_to
+buildit format_to_n
+buildit integer-format
+buildit locale
+buildit pointer-format
+buildit repeated-param
+buildit sign-zero
+buildit string-format
+buildit unused-args
+buildit vlog
+buildit widths
